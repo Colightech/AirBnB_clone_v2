24b5c2a89350979dfb7fb183c51670aad6b749d9
Colightech <saintchris4@gmail.com>
2024-01-17 13:16:35 -0800

    AirBnB_clone_v2

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from sqlalchemy import Column, String, Table, ForeignKey
-from sqlalchemy.orm import relationship
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
-
-# Define the association table for the Many-to-Many relationship
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False)
-                      )
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity class """
-    __tablename__ = 'amenities'
-
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-
-    """ Define the Many-to-Many relationship with the Place class """
-    place_amenities = relationship('Place', secondary=place_amenity, back_populates='amenities')
+    place_amenities = relationship("Place", secondary=place_amenity)
